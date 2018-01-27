dcd3840d1e9b33cc3a2e92de0d426ce0074afe14
Edgar Lee <edgar.lee@docker.com>
2018-01-26 16:04:47 -0800

    Push converted manifest blobs

--- cmd/v1tov2/main.go
+++ cmd/v1tov2/main.go
@@ -2,15 +2,14 @@ package main
 
 import (
 	"context"
+	"io"
 	"log"
 	"net/http"
 
+	"github.com/containerd/containerd/content"
 	"github.com/containerd/containerd/content/local"
-	"github.com/containerd/containerd/images"
 	"github.com/containerd/containerd/remotes/docker"
 	"github.com/containerd/containerd/remotes/docker/schema1"
-	digest "github.com/opencontainers/go-digest"
-	ocispec "github.com/opencontainers/image-spec/specs-go/v1"
 	"github.com/palantir/stacktrace"
 )
 
@@ -22,11 +21,6 @@ func main() {
 }
 
 func run() error {
-	contentStore, err := local.NewStore("/tmp/store")
-	if err != nil {
-		return stacktrace.Propagate(err, "failed to create local content store")
-	}
-
 	resolver := docker.NewResolver(docker.ResolverOptions{
 		Credentials: nil,
 		PlainHTTP:   true,
@@ -34,23 +28,24 @@ func run() error {
 	})
 
 	ctx := context.Background()
-	ref := "172.17.0.1:5000/repo/alpine"
+	ref := "172.17.0.1:5000/repo/alpine:v1"
 	fetcher, err := resolver.Fetcher(ctx, ref)
 	if err != nil {
 		return stacktrace.Propagate(err, "failed to create fetcher for '%s'", ref)
 	}
 
+	contentStore, err := local.NewStore("/tmp/store")
+	if err != nil {
+		return stacktrace.Propagate(err, "failed to create local content store")
+	}
+
 	converter := schema1.NewConverter(contentStore, fetcher)
 
-	dgst, err := digest.Parse("sha256:a2938f1d34192c41489111c22320fb7972e65862f1e841621f32dccb56802b57")
+	_, desc, err := resolver.Resolve(ctx, ref)
 	if err != nil {
-		return stacktrace.Propagate(err, "failed to parse digest")
+		return stacktrace.Propagate(err, "failed to resolve reference '%s'", ref)
 	}
 
-	desc := ocispec.Descriptor{
-		Digest:    dgst,
-		MediaType: images.MediaTypeDockerSchema1Manifest,
-	}
 	v1descs, err := converter.Handle(ctx, desc)
 	if err != nil {
 		return stacktrace.Propagate(err, "failed to pull manifest for digest '%s'", desc.Digest)
@@ -62,6 +57,29 @@ func run() error {
 		return stacktrace.Propagate(err, "failed to convert v1 manifest for '%s' to v2 manifest", desc.Digest)
 	}
 	log.Printf("v2desc: %s\n", v2desc)
+
+	pusher, err := resolver.Pusher(ctx, ref)
+	if err != nil {
+		return stacktrace.Propagate(err, "failed to create pusher for '%s'", ref)
+	}
+
+	writer, err := pusher.Push(ctx, v2desc)
+	if err != nil {
+		return stacktrace.Propagate(err, "failed to create writer for pushing blob '%s'", v2desc.Digest.String())
+	}
+	defer writer.Close()
+
+	readerAt, err := contentStore.ReaderAt(ctx, v2desc.Digest)
+	if err != nil {
+		return stacktrace.Propagate(err, "failed to create reader at")
+	}
+	defer readerAt.Close()
+
+	sectionReader := io.NewSectionReader(readerAt, 0, v2desc.Size)
+	err = content.Copy(ctx, writer, sectionReader, v2desc.Size, v2desc.Digest)
+	if err != nil {
+		return stacktrace.Propagate(err, "failed to copy blob from local content store to pusher")
+	}
 
 	return err
 }
