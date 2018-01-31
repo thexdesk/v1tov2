d3fada67cc897db0ce627deb2a9ab38fd0381365
Edgar Lee <edgar.lee@docker.com>
2018-01-31 13:15:22 -0800

    Use future interfaces from containerd PRs 2075 and 2082

--- Makefile
+++ Makefile
@@ -1,48 +1,7 @@
-BINARIES=bin/v1tov2
-
-GOFILES=$(shell find . -type f -name '*.go')
-
-DOCKERFILES=$(shell find . -type f -name 'Dockerfile')
-
-BUILDKIT_HOST="tcp://0.0.0.0:1234"
-
-.DEFAULT: all
-all: v1tov2
-
-binaries: $(BINARIES)
-
-bin/%: build
+.DEFAULT: v1tov2
+v1tov2:
 	@echo "+ $@"
+	@docker build -t hinshun/v1tov2 .
 
-buildkit:
-	@echo "+ $@"
-	@docker inspect buildkit > /dev/null || docker run -d --name buildkit --privileged -p 1234:1234 tonistiigi/buildkit:latest --debug --addr ${BUILDKIT_HOST}
-
-build: buildkit $(GOFILES)
-	@echo "+ $@"
-	@go get -u github.com/moby/buildkit/cmd/buildctl
-	@go run cmd/buildkit-lemming/main.go | BUILDKIT_HOST=${BUILDKIT_HOST} buildctl build --local src=. --exporter local --exporter-opt output=./bin
-
-lemming: bin/lemming $(DOCKERFILES)
-	@echo "+ $@"
-	@BUILDKIT_HOST=${BUILDKIT_HOST} buildctl build --frontend=dockerfile.v0 --local context=. --local dockerfile=cmd/lemming --exporter=docker --exporter-opt name=docker/lemming | docker load
-
-lemmingd: bin/lemmingd $(DOCKERFILES)
-	@echo "+ $@"
-	@BUILDKIT_HOST=${BUILDKIT_HOST} buildctl build --frontend=dockerfile.v0 --local context=. --local dockerfile=cmd/lemmingd --exporter=docker --exporter-opt name=docker/lemmingd | docker load
-
-lemmingctl: bin/lemmingctl $(DOCKERFILES)
-	@echo "+ $@"
-	@BUILDKIT_HOST=${BUILDKIT_HOST} buildctl build --frontend=dockerfile.v0 --local context=. --local dockerfile=cmd/lemmingctl --exporter=docker --exporter-opt name=docker/lemmingctl | docker load
-
-clean:
-	@echo "+ $@"
-	@docker rm -f buildkit
-	@rm -rf "./bin"
-
-vendor:
-	@echo "+ $@"
-	@./hack/update-vendor
-
-.PHONY: buildkit build lemming lemmingd lemmingctl clean vendor
+.PHONY: v1tov2
 
--- cmd/v1tov2/main.go
+++ cmd/v1tov2/main.go
@@ -2,12 +2,12 @@ package main
 
 import (
 	"context"
-	"io"
 	"log"
 	"net/http"
 
-	"github.com/containerd/containerd/content"
 	"github.com/containerd/containerd/content/local"
+	"github.com/containerd/containerd/images"
+	"github.com/containerd/containerd/remotes"
 	"github.com/containerd/containerd/remotes/docker"
 	"github.com/containerd/containerd/remotes/docker/schema1"
 	"github.com/palantir/stacktrace"
@@ -46,44 +46,25 @@ func run() error {
 		return stacktrace.Propagate(err, "failed to resolve reference '%s'", ref)
 	}
 
-	v1descs, err := converter.Handle(ctx, desc)
+	err = images.Dispatch(ctx, converter, desc)
 	if err != nil {
 		return stacktrace.Propagate(err, "failed to pull manifest for digest '%s'", desc.Digest)
 	}
-	log.Printf("v1descs: %s\n", v1descs)
 
-	v2desc, err := converter.Convert(ctx)
+	v2desc, err := converter.Convert(ctx, schema1.ConvertToDockerSchema2Manifest())
 	if err != nil {
 		return stacktrace.Propagate(err, "failed to convert v1 manifest for '%s' to v2 manifest", desc.Digest)
 	}
-	log.Printf("v2desc: %s\n", v2desc)
 
 	pusher, err := resolver.Pusher(ctx, ref)
 	if err != nil {
 		return stacktrace.Propagate(err, "failed to create pusher for '%s'", ref)
 	}
 
-	writer, err := pusher.Push(ctx, v2desc)
+	err = remotes.PushContent(ctx, contentStore, pusher, nil, v2desc)
 	if err != nil {
-		return stacktrace.Propagate(err, "failed to create writer for pushing blob '%s'", v2desc.Digest.String())
-	}
-	defer writer.Close()
-
-	readerAt, err := contentStore.ReaderAt(ctx, v2desc.Digest)
-	if err != nil {
-		return stacktrace.Propagate(err, "failed to create reader at")
-	}
-	defer readerAt.Close()
-
-	sectionReader := io.NewSectionReader(readerAt, 0, v2desc.Size)
-	err = content.Copy(ctx, writer, sectionReader, v2desc.Size, v2desc.Digest)
-	if err != nil {
-		return stacktrace.Propagate(err, "failed to copy blob from local content store to pusher")
+		return stacktrace.Propagate(err, "failed to push blobs")
 	}
 
 	return err
-}
-
-func credentials(host string) (username string, secret string, err error) {
-	return "", "token", nil
 }
